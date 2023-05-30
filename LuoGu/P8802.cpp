329d1981a6f2b01756a626d02fd9ca1d5b2cd994
柠檬味的蓝莓派 <1097314806@qq.com>
2023-05-30 23:07:01 +0800

    Update solution

--- LuoGu/P8802.cpp
+++ LuoGu/P8802.cpp
@@ -2,26 +2,25 @@
 #define endl "\n"
 using namespace std;
 
-const int N = 1005, M = 10005;
-int C[N + 1], cost[N + 1][N + 1], dis[N + 1], vis[N + 1], n, m;
+const int N = 1005;
+int n, m, cost[N][N], C[N], dis[N], vis[N];
 
 int dijkstra() {
     memset(dis, 0x3f, sizeof(dis));
-    memset(vis, 0, sizeof(vis));
     dis[1] = 0;
     int loop = n - 1;
     while (loop--) {
-        int cur = -1;
-        for (int i = 1; i <= n; i++) {
-            if (!vis[i] && (cur == -1 || dis[cur] > dis[i])) {
-                cur = i;
+        int t = -1;
+        for (int j = 1; j <= n; j++) {
+            if (!vis[j] && (t == -1 || dis[t] > dis[j])) {
+                t = j;
             }
         }
-        if (cur == n) break;
-        for (int i = 1; i <= n; i++) {
-            dis[i] = min(dis[cur], dis[cur] + cost[cur][i]);
+        if (t == n) break;
+        for (int j = 1; j <= n; j++) {
+            dis[j] = min(dis[j], dis[t] + cost[t][j]);
         }
-        vis[cur] = 1;
+        vis[t] = 1;
     }
     return dis[n];
 }
@@ -31,18 +30,16 @@ void solve() {
     for (int i = 1; i <= n; i++) {
         cin >> C[i];
     }
+    memset(cost, 0x3f, sizeof(cost));
     for (int i = 1, u, v, c; i <= m; i++) {
         cin >> u >> v >> c;
         cost[u][v] = c + C[v];
         cost[v][u] = c + C[u];
     }
-    cout << dijkstra() << endl;
+    cout << dijkstra() - C[n] << endl;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
     int t = 1;
     // cin >> t;
     while (t--) {
