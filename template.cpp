8695ecd966f93944591aa40c920cead0c9eeac44
柠檬味的蓝莓派 <1097314806@qq.com>
2023-06-19 19:01:51 +0800

    Add fast power algo

--- template.cpp
+++ template.cpp
@@ -3,12 +3,23 @@
 #define ll long long
 using namespace std;
 
-inline int read() {
+inline int read() {     //快读
     char ch = '*';
     while (!isdigit(ch = getchar()));
     int num = ch - '0';
     while (isdigit(ch = getchar())) num = num * 10 + ch - '0';
     return num;
+}
+
+ll fPow(ll x, ll n) {  //快速幂
+    ll result = 1;
+    while (n > 0) {
+        if (n & 1)
+            result *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return result;
 }
 
 void solve() {
