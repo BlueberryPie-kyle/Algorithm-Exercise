04b77ece9bf034b59b94b2e59e7b4af1ad89dba7
柠檬味的蓝莓派 <1097314806@qq.com>
2023-06-22 19:37:42 +0800

    Update solution

--- Codeforces/1843(Round 881)/1843D.cpp
+++ Codeforces/1843(Round 881)/1843D.cpp
@@ -5,24 +5,39 @@ using namespace std;
 
 const int N = 2e5 + 5;
 
-struct ChainFowardStar {
-    int to;
-    int from;
-    int next;
-} edge[N * N];
+int n, q, ch[N];
+vector<int> g[N];
 
-int head[N], cnt;
-
-void addEdge(int from, int to) {
-    edge[cnt].to = to;
-    edge[cnt].from = from;
-    edge[cnt].next = head[from];
-    head[from] = cnt++;
+int dfs(int u, int p) {
+    ch[u] = 0;
+    bool ok = false;
+    for (auto &i: g[u]) {
+        if (i == p) continue;
+        ch[u] += dfs(i, u);
+        ok = true;
+    }
+    return ok ? ch[u] : ch[u] = 1;
 }
 
-void solve() {
-    memset(head, -1, sizeof head);
-    
+void solve() {  //邻接表
+    cin >> n;
+    memset(ch, 0, sizeof ch);
+    for (int i = 0; i < N; i++) {
+        g[i].clear();
+    }
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    dfs(1, 0);
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int x, y;
+        cin >> x >> y;
+        cout << (ll) ch[x] * ch[y] << endl;
+    }
 }
 
 int main() {
