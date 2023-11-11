26a4019913d37851867e09a377592f3127a5d80f
柠檬味的蓝莓派 <1097314806@qq.com>
2023-11-11 22:09:57 +0800

    Fix some grammar mistakes and add cout redirect

--- MatchingTemplate.cpp
+++ MatchingTemplate.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 // if you use "freopen"
 // your "cin" and "cout" will read from file what you point it is "r"
-// and write it in file what you point it is "w"
+// and be writen in file what you point it is "w"
+
+// Start from this:
 
 void randomBuilder() {
     freopen("..\\TestCases\\testcase.in", "w", stdout);
@@ -23,6 +25,8 @@ void correctProgram() {
     /* code */
 }
 
+// End of this. Code above ought to be writen in other file, there's just a template
+
 void checker() {
     int t = 1;// this is the number of test you want to do
     cin >> t;
@@ -30,7 +34,9 @@ void checker() {
         system("your random.exe");        // this is random testcase builder
         system("your testProgram.exe");   // input program's path in it
         system("your correctProgram.exe");// don't forget compile before check!
-
+        cout.flush();                     // clear the buffer
+        freopen("CON", "r", stdin);       // redirect stdin to console
+        freopen("CON", "w", stdout);      // redirect stdout to console
         if (system("fc .\\TestCases\\${text ans}.ans .\\TestCases\\correct.out")) {
             cout << "WA for the" << t << "testcases" << endl;
             return;
